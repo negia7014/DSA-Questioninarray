74be6b4eb0e8ac93549cd1e5496ee119be03e5fa
ABHISHEK NEGI <111852535+negia7014@users.noreply.github.com>
2024-08-31 12:14:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock buy and sell II/stock-buy-and-sell-ii.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] prices = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.stockBuyAndSell(n, prices);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        int maxprofit = 0;
+        int profit;
+        int buy = prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(buy<prices[i]){
+                profit = prices[i]-buy;
+                maxprofit +=profit;
+                buy = prices[i];
+            }else{
+                buy = prices[i];
+                continue;
+            }
+        }
+     return maxprofit;}
+}
+        
