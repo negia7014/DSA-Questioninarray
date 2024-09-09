5c36697f1964ba0b988c602eafe380104c1c1f76
ABHISHEK NEGI <111852535+negia7014@users.noreply.github.com>
2024-09-09 18:04:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.print2largest(arr);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int print2largest(int[] arr) {
+        // Code Here
+        int largest=arr[0];
+        int seclargest=-1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                seclargest = largest;
+                largest = arr[i];
+            }else if(arr[i]<largest && arr[i]>seclargest){
+                seclargest = arr[i];
+            }
+            
+        }
+        return seclargest;
+    }
+}
