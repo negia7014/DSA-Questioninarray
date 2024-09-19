597c99cd3443424fb51225de20eae94f36dc29e4
ABHISHEK NEGI <111852535+negia7014@users.noreply.github.com>
2024-09-19 19:36:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().majorityElement(arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int majorityElement(int arr[]) {
+        // your code here
+        int winnelement = 0;
+        int freq = 0;
+        for(int i=0;i<arr.length;i++){
+            if(freq==0){
+                winnelement = arr[i];
+            }
+            if(winnelement==arr[i]){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==winnelement){
+                count++;
+            }
+        }
+        if(count>arr.length/2){
+            return winnelement;
+        }else{
+            return -1;
+        }
+        
+    }
+}
